30fa217857a777da606305e491ce35f561e29ac6
thuantruong00 <46294831+thuantruong00@users.noreply.github.com>
2021-05-17 15:45:34 +0700

    90% index

---
+++ script/slider.js
@@ -0,0 +1,21 @@
+$('#num1').owlCarousel({
+    margin:0,
+    loop:true,
+    autoplay:true,
+    autoplayTimeout:1500,
+    autoplayHoverPause:true,
+    smartSpeed:2000,
+    dots:true,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
